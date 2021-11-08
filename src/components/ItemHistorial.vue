b3d9d29efc943c2121a857a7261ebe1f79facc65
Emmanuel Capandegui <ecapandegui@gmail.com>
2021-11-08 02:24:14 -0300

    Agregamos los componentes de ficha medica e historial con datos hardcodeados

---
+++ src/components/ItemHistorial.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="ItemRegistro">
+        <div class="ItemRegistro__Dia">
+            {{dia}}
+        </div>
+        <div class="ItemRegistro__Descripcion">
+            {{descripcion}}
+        </div>
+    </div>
+</template>
+<script>
+export default {
+  name: 'ItemHistorial',
+  props: {
+    dia: String,
+    descripcion: String
+  },
+  data:() => {
+      return{
+          
+      }
+  }
+}
+</script>
