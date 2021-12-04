0697f73134de703bdf93d0697b9acf8b28d02799
Emmanuel Capandegui <ecapandegui@gmail.com>
2021-12-04 20:10:41 -0300

    cambio en la versio de vuex y modificacion al store

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
  import { createApp } from 'vue'
- //import Vue from 'vue'
+//  import Vue from 'vue'
 import App from './App.vue'
 import router from './router';
-//import store from './store';
+import store from './store';
 
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 
@@ -10,19 +10,32 @@ import router from './router';
  import 'bootstrap';
 
   const app = createApp(App)
- app.use(router);
-// app.use(store);
+  app.use(router);
+  app.use(store);
+  app.mount('#app')
 
- app.mount('#app')
+ //
 
 
-//Vue.config.productionTip = false
+
+
+
+
+
+ //Vue.config.productionTip = false
 
 /* eslint-disable no-new */
-/*new Vue({
-  el: '#app',
-  router,
-  store,
-  template: '<App/>',
-  components: { App }
-})*/
+// new Vue({
+//   el: '#app',
+//   router,
+//   store,
+//   template: '<App/>',
+//   components: { App }
+// })
+//*********** */
+// new Vue({
+//   router,
+//   store,
+//   render: h => h(App)
+//   // components: { App }
+// }).$mount('#app')
--- src/store/index.js
+++ src/store/index.js
@@ -1,32 +1,48 @@
+import { createStore } from 'vuex'
 
-//import Vue from 'vue'
-import Vuex from 'vuex';
+// Create a new store instance.
+const store = createStore({
+  state () {
+    return {
+      usuario:{
+          nombre:"Emmanuel",
+          apellido:"Capandegui",
+          fechaNacimiento:"",
 
-//Vue.use(Vuex);
-
-export default new Vuex.Store({
-    state:{
-        usuario:{
-            nombre:"Emmanuel",
-            apellido:"Capandegui",
-            fechaNacimiento:"",
-
-        },
-        llamada:{
-            error:false,
-            metodo:""
-        },
-        loading:false,
-
-
-    },
-    mutations:{
-
-    },
-    actions:{
-
-    },
-    getters:{
-
+      },
+      llamada:{
+          error:false,
+          metodo:""
+      },
+      loading:false,
     }
-});
+  },
+  mutations: {
+    increment (state) {
+      state.count++
+    }
+  }
+})
+
+export default store;
+// import Vue from 'vue'
+// import Vuex from 'vuex';
+
+// Vue.use(Vuex);
+
+// export default new Vuex.Store({
+//     state:{
+//        
+
+
+//     },
+//     mutations:{
+
+//     },
+//     actions:{
+
+//     },
+//     getters:{
+
+//     }
+// });
